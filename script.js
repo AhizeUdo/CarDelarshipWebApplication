1cf1e40fed83628e221c29ee9bd2659ab061c00f
Vladhex12 <cloutsfor@gmail.com>
2022-10-08 03:10:29 +0100

    Navigation Fix

--- script.js
+++ script.js
@@ -3,6 +3,15 @@ const navbarlinks = document.getElementsByClassName('hamburger-navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
     navbarlinks.classList.toggle('active')
+    closenavmain.style.display = 'flex'
+    // closenavmain.style.width = 
 });
 
 // alert('Welcome')
+
+const closenav = document.getElementById('closeXnavigation');
+const closenavmain = document.getElementById('navpackethidden');
+
+closenav.addEventListener('click', () => {
+    closenavmain.style.display = 'none'
+});
