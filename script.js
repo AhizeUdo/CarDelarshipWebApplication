0151767e993dd7ffc3b59958a67245113d3a6568
Vladhex12 <cloutsfor@gmail.com>
2022-10-16 01:09:19 +0100

    Dom Contacts Build

--- script.js
+++ script.js
@@ -15,3 +15,31 @@ const closenavmain = document.getElementById('navpackethidden');
 closenav.addEventListener('click', () => {
     closenavmain.style.display = 'none'
 });
+
+
+const hidden_section_parent = document.getElementById('hidden-section-parent');
+const hidden_section = document.querySelector('.hidden-section');
+hidden_section.style.display = 'block';
+hidden_section.style.backgroundColor = 'red';
+hidden_section.style.height = '300px'; 
+hidden_section.style.color = 'white';
+
+
+
+const contactFormfirstname = document.createElement('label');
+contactFormfirstname.for = 'lname';
+contactFormfirstname.type = 'text';
+contactFormfirstname.name = 'firstname';
+contactFormfirstname.placeholder = 'Your name...'
+
+const contactFormlastname = document.createElement('label');
+contactFormlastname.for = 'lname';
+contactFormlastname.type = 'text';
+contactFormlastname.name = 'firstname';
+contactFormlastname.placeholder = 'Your last name...'
+
+hidden_section.appendChild(contactFormlastname)
+hidden_section.appendChild(contactFormfirstname)
+
+console.log(hidden_section)
+console.log(contactFormlastname)
